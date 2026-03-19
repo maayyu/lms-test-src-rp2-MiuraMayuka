f09c69d754d7a53ec155788cc44be86a0d9efe06
m-miura <m-miura@k-shared.co.jp>
2026-03-19 15:34:06 +0900

    fix:Case02受講生ログイン認証失敗 テストコード追加
    WebElementを利用してエラーメッセージの値を取得、期待値通りかどうか確認するテストを作成
    URL・見出し・入力フォームの表示確認とスクリーンショットによる保存を実装
    期待値は以下の通り
    URL：http://localhost:8080/lms/login
    タイトル：ログイン | LMS
    見出し：ログイン
    ログインID：入力欄が表示されていること／入力値が残っていること（123456789）
    パスワード：入力欄が表示されていること／入力値がクリアされていること
    エラーメッセージ：* ログインに失敗しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,61 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//		指定URLへ遷移
+		webDriver.get("http://localhost:8080/lms/");
+
+		// タイトルの検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// 見出しの検証
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+		// ログインIDフォームの確認
+		assertTrue(webDriver.findElement(By.id("loginId")).isDisplayed());
+
+		// パスワードフォームの確認
+		assertTrue(webDriver.findElement(By.id("password")).isDisplayed());
+
+		// スクリーンショット
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//	ログインID入力
+		webDriver.findElement(By.id("loginId")).clear();
+		webDriver.findElement(By.id("loginId")).sendKeys("123456789");
+
+		//	パスワード入力
+		webDriver.findElement(By.id("password")).clear();
+		webDriver.findElement(By.id("password")).sendKeys("123456789");
+
+		//	ログインボタンを押下
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		// ページが更新されるまで待機
+		pageLoadTimeout(10);
+
+		// ログイン画面に遷移しているかURL確認
+		assertTrue(webDriver.getCurrentUrl().startsWith("http://localhost:8080/lms/login"));
+
+		//	エラーメッセージの確認
+		WebElement errorMessageElement = webDriver.findElement(By.className("error"));
+		assertEquals("* ログインに失敗しました。", errorMessageElement.getText());
+
+		// ログインID入力欄に入力値が残っているかを確認
+		assertEquals("123456789", webDriver.findElement(By.id("loginId")).getAttribute("value"));
+
+		// パスワード入力欄の入力値がクリアされているかを確認
+		assertEquals("", webDriver.findElement(By.id("password")).getAttribute("value"));
+
+		// スクリーンショット
+		getEvidence(new Object() {
+		});
 	}
 
 }
