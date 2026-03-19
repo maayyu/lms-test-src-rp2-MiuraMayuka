25fdaca02a096eab8755c73844a2f6822da5003d
m-miura <m-miura@k-shared.co.jp>
2026-03-19 12:02:31 +0900

    fix:Case01 ログイン画面への遷移テストコード追加
    WebElementを利用してタイトルの値とURL・見出し・ログインID入力欄・パスワード入力欄の表示確認をするテストを作成した。また、スクリーンショットによる証跡保存。
    期待値は以下の通り
    URL：http://localhost:8080/lms/
    タイトル：ログイン | LMS
    見出し：ログイン
    ログインID：入力欄が表示されていること
    パスワード：入力欄が表示されていること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,7 +37,24 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//	指定URLへ遷移
+		goTo("http://localhost:8080/lms/");
+
+		// タイトルの検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// 見出しの検証
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+		// ログインIDフォームの確認
+		assertTrue(webDriver.findElement(By.id("loginId")).isDisplayed());
+
+		// パスワードフォームの確認
+		assertTrue(webDriver.findElement(By.id("password")).isDisplayed());
+
+		// スクリーンショット
+		getEvidence(new Object() {
+		});
 	}
 
 }
